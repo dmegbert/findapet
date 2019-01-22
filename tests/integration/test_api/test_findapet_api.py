4a6dcecba808e21f55354ec0b87828208be6e921
David Egbert <dmegbert@gmail.com>
2019-01-22 15:12:56 -0500

    testing and zappa deployment working...

---
+++ tests/integration/test_api/test_findapet_api.py
@@ -0,0 +1,13 @@
+import findapet_api
+
+
+def test_index():
+    tester = findapet_api.app.test_client()
+    response = tester.get('/', content_type='html/text')
+    assert response.status_code==200
+    assert b'Glen of Imaal Terrier' in response.data
+
+
+
+
+
